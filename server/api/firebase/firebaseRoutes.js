bdb9b9ab872aa97d41a9619bd7b1e5aa2c75e57a
Patrick Sheehan <psheehan89@gmail.com>
2017-02-15 11:23:51 -0800

    (feat) Created firebase api directory on the server-side. Pasted basic firebase config settings in firebaseRoutes.js

---
+++ server/api/firebase/firebaseRoutes.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBYzcVnUFZiLsR-XDhCKWMNVVw4sEN3syc",
+    authDomain: "rabble-984ed.firebaseapp.com",
+    databaseURL: "https://rabble-984ed.firebaseio.com",
+    storageBucket: "rabble-984ed.appspot.com",
+    messagingSenderId: "869569787907"
+  };
+  firebase.initializeApp(config);
