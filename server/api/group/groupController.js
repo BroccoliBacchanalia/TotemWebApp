c11194aa3ea5976193af919df6e27f6282622bdb
Derek Young <dthomasy@gmail.com>
2017-02-19 22:53:29 -0800

    Group routes

--- server/api/group/groupController.js
+++ server/api/group/groupController.js
@@ -6,7 +6,18 @@ const controller = {
     //TODO
   },
   create: function(req, res, next) {
-    //TODO
+    console.log('group post req received');
+    console.log(req.body);
+
+    Group.create({
+      name: req.body.groupName
+    }).then(function(group, created) {
+      console.log('group created, id: ', group.dataValues.id);
+      return res.json(group.dataValues.id);
+    }).catch(function(err) {
+      console.log('error in create user:', err.errors[0].message);
+      return res.status(500).send(err.errors[0].message);
+    });
   }
 };
 
