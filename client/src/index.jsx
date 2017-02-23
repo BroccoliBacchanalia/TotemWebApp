9b7a3faea76075a9b36662387c156dbbe3a7960e
John Currie <johntcurrie@gmail.com>
2017-02-23 12:16:06 -0800

    Start refactor to React

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { browserHistory, Router, Route } from 'react-router';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+//import { connect } from 'react-redux';
+import { Route, hashHistory, Router } from 'react-router';
+//import store from './store.js';
+//import styles from './styles';
+//import { Provider } from 'react-redux';
 import App from './components/App.jsx';
-import store from './store.jsx';
 
-render(
-  <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path='/' component={App}>
-      </Route>
-    </Router>
-  </Provider>, document.getElementById('app'));
+/*  Components  */
+// import NavMenu from './components/Nav/NavMenu';
+// import MapViewer from './components/MapView/MapView';
+// import Group from './components/Group/Group';
+// import VenueSchedule from './components/VenueSchedule/VenueSchedule';
+// import ChooseVenue from './components/InitConfig/ChooseVenue';
+// import InviteFriends from './components/InitConfig/InviteFriends';
+// import CreateGroup from './components/InitConfig/CreateGroup';
+ReactDOM.render(
+<Router history={ hashHistory} >
+	          <Route path="/" component={App}/>
+	          <Route path="/group" component={() => (
+	            // <Group
+	              // dispatch={this.props.dispatch}
+	              // users={this.props.location.users}
+	              // userID={this.props.app.userFbId}
+	              <div>User Schedule Holder</div>
+	            // />
+	          )}/>
+	          <Route path="/agenda" component={() => <div>User Schedule Holder</div>}/>
+	          <Route path="/schedule" component={() => <div>User Schedule Holder</div>}/>
+	          <Route path="/emergency" component={() => <div>Emergency Info Holder</div>}/>
+	          <Route path="/choosevenue" component={() => <div>User Schedule Holder</div>}/>
+	          <Route path="/create" component={() => <div>User Schedule Holder</div>}/>
+	          <Route path="/invite" component={() => <div>User Schedule Holder</div>}/>
+</Router>
+  , document.getElementById('app')
+)
+
+// export default connect((store) => {
+//   return {
+//     app: store.app,
+//     nav: store.nav,
+//     location: store.location
+//   };
+// })(App);
--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 'use strict'
 const express = require('express');
-const api = require('./api/api');
+// const api = require('./api/api');
 const app = express();
 const port = process.env.PORT || 8000;
 
 require('./middleware')(app, express);
 
-app.use('/api', api);
+// app.use('/api', api);
 
 app.listen(port, function() {
   console.log('listening on port ' + port);
