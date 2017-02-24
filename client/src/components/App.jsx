e3d435e651090ce99a2fb578ff02655b29d01332
Derek Young <dthomasy@gmail.com>
2017-02-23 17:18:21 -0800

    (feat) Css modularity and webpack minification

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -19,21 +19,27 @@ class App extends React.Component {
       <Router>
         <div>
           <NavigationBar />
-  				<Route exact path="/" component={() => <div>Map Holder</div>}/>
-  					<Route path="/group" component={() => (
-  						<Group
-  							dispatch={this.props.dispatch}
-  							users={this.props.location.users}
-  							userID={this.props.app.userFbId}
-  						/>
-  					)}/>
-            <Route path="/map" component={MapViewer}/>
-  					<Route path="/agenda" component={() => <div>Agenda Holder</div>}/>
-  					<Route path="/schedule" component={() => <div>Schedule Holder</div>}/>
-  					<Route path="/emergency" component={() => <div>Emergency Emergency Info Holder</div>}/>
-  					<Route path="/choosevenue" component={() => <div>Venue Holder</div>}/>
-  					<Route path="/create" component={() => <div>Create Holder</div>}/>
-  					<Route path="/invite" component={() => <div>Invite Holder</div>}/>
+  				<Route exact path="/" component={() => (
+            <Group
+              dispatch={this.props.dispatch}
+              users={this.props.location.users}
+              userID={this.props.app.userFbId}
+            />
+          )}/>
+					<Route path="/group" component={() => (
+						<Group
+							dispatch={this.props.dispatch}
+							users={this.props.location.users}
+							userID={this.props.app.userFbId}
+						/>
+					)}/>
+          <Route path="/map" component={MapViewer}/>
+					<Route path="/agenda" component={() => <div>Agenda Holder</div>}/>
+					<Route path="/schedule" component={() => <div>Schedule Holder</div>}/>
+					<Route path="/emergency" component={() => <div>Emergency Emergency Info Holder</div>}/>
+					<Route path="/choosevenue" component={() => <div>Venue Holder</div>}/>
+					<Route path="/create" component={() => <div>Create Holder</div>}/>
+					<Route path="/invite" component={() => <div>Invite Holder</div>}/>
         </div>
 			</Router>
     );
--- client/src/components/Group/GroupRow.jsx
+++ client/src/components/Group/GroupRow.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import localStyles from './GroupStyles.css';
 // import { getGeofence } from '../../actions/locationActions.jsx'
 
 const GroupRow = ({ friend }) => (
-  <div>
-    hi
-  </div> {//Thin line}
+  <div className={localStyles.main}>
+    <img src={friend.img} width="50px"/>
+    {console.log(friend)}
+  </div> //Thin line
 );
 
 export default GroupRow;
