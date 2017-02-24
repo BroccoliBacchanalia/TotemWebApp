a67080f5e4af0ed4cf7bbe8af435445e3406db54
Derek Young <dthomasy@gmail.com>
2017-02-23 23:15:34 -0800

    (feat) Image ground overlay

---
+++ GroundOverlay.js
@@ -0,0 +1,124 @@
+"use strict";
+
+var _defineProperty2 = require("babel-runtime/helpers/defineProperty");
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
+var _extends2 = require("babel-runtime/helpers/extends");
+
+var _extends3 = _interopRequireDefault(_extends2);
+
+var _delay2 = require("lodash/delay");
+
+var _delay3 = _interopRequireDefault(_delay2);
+
+var _flowRight2 = require("lodash/flowRight");
+
+var _flowRight3 = _interopRequireDefault(_flowRight2);
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+var _constants = require("./node_modules/react-google-maps/lib/constants");
+
+var _enhanceElement = require("./node_modules/react-google-maps/lib/enhanceElement");
+
+var _enhanceElement2 = _interopRequireDefault(_enhanceElement);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* global google */
+var controlledPropTypes = {
+  // NOTICE!!!!!!
+  //
+  // Only expose those with getters & setters in the table as controlled props.
+  //
+  // [].map.call($0.querySelectorAll("tr>td>code", function(it){ return it.textContent; })
+  //    .filter(function(it){ return it.match(/^set/) && !it.match(/^setMap/); })
+  //
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#OverlayView
+  clickable: _react.PropTypes.bool,
+  opacity: _react.PropTypes.number,
+};
+
+var defaultUncontrolledPropTypes = (0, _enhanceElement.addDefaultPrefixToPropTypes)(controlledPropTypes);
+
+var eventMap = {
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#OverlayView
+  // [].map.call($0.querySelectorAll("tr>td>code"), function(it){ return it.textContent; })
+  onClick: `click`,
+  onDblClick: `dblclick`,
+};
+
+var publicMethodMap = {
+  // Public APIs
+  //
+  // https://developers.google.com/maps/documentation/javascript/3.exp/reference#OverlayView
+  //
+  // [].map.call($0.querySelectorAll("tr>td>code"), function(it){ return it.textContent; })
+  //    .filter(function(it){ return it.match(/^get/) && !it.match(/Map$/); })
+
+  getBounds: function getBounds(groundOverlay) {
+    return groundOverlay.getBounds();
+  },
+
+  getMap: function getMap(groundOverlay) {
+    return groundOverlay.getMap();
+  },
+
+  getOpacity: function getOpacity(groundOverlay) {
+    return groundOverlay.getOpacity();
+  },
+
+  getUrl: function getUrl(groundOverlay) {
+    return groundOverlay.getUrl();
+  },
+};
+
+var controlledPropUpdaterMap = {};
+
+function getInstanceFromComponent(component) {
+  return component.state[_constants.GROUND_OVERLAY];
+}
+
+module.exports = (0, _flowRight3.default)(_react2.default.createClass, (0, _enhanceElement2.default)(getInstanceFromComponent, publicMethodMap, eventMap, controlledPropUpdaterMap))({
+  displayName: "GroundOverlay",
+
+  statics: {
+    CLICKABLE: "clickable",
+    MAP: "map",
+    OPACITY: "opacity"
+  },
+
+  propTypes: (0, _extends3.default)({}, controlledPropTypes, defaultUncontrolledPropTypes, {
+    //None
+  }),
+
+  contextTypes: (0, _defineProperty3.default)({}, _constants.MAP, _react.PropTypes.object),
+
+  getInitialState: function getInitialState() {
+    // https://developers.google.com/maps/documentation/javascript/3.exp/reference#OverlayView
+    console.log(this.props);
+    var groundOverlay = new google.maps.GroundOverlay(this.props.image, this.props.imageBounds);
+
+    // You must call setMap() with a valid Map object to trigger the call to
+    // the onAdd() method and setMap(null) in order to trigger the onRemove() method.
+    groundOverlay.setMap(this.context[_constants.MAP]);
+    if (this.props.opacity) {
+      groundOverlay.setOpacity(this.props.opacity);
+    }
+    return (0, _defineProperty3.default)({}, _constants.OVERLAY_VIEW, groundOverlay);
+  },
+  componentWillUnmount: function componentWillUnmount() {
+    var groundOverlay = getInstanceFromComponent(this);
+    if (groundOverlay) {
+      groundOverlay.setMap(null);
+    }
+  },
+  render: function render() {
+    return false;
+  }
+});
--- client/src/components/MapViewer/MapViewer.jsx
+++ client/src/components/MapViewer/MapViewer.jsx
@@ -1,9 +1,9 @@
 import { connect } from 'react-redux';
 import _ from 'lodash';
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker, OverlayView } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import GroundOverlay from '../../../../GroundOverlay';
 import Helmet from "react-helmet";
-
 
 /*
  * This is the modify version of:
@@ -12,10 +12,7 @@ import Helmet from "react-helmet";
  * Add <script src="https://maps.googleapis.com/maps/api/js"></script> to your HTML to provide google.maps reference
  */
 
-
 class MapViewer extends Component {
-
-
   state = {
     markers: [{
       position: {
@@ -47,7 +44,6 @@ class MapViewer extends Component {
     this._mapComponent = map;
     if (map) {
       console.log(map.getZoom());
-      console.log(this.props)
     }
   }
 
@@ -59,107 +55,38 @@ class MapViewer extends Component {
     const STYLES = {
       mapContainer: {
         height: `100%`,
-      },
-      overlayView: {
-        background: `white`,
-        border: `1px solid #ccc`,
-        padding: 15,
-      },
+      }
     };
-
 
     const LoadMap = withGoogleMap(props => (
       <GoogleMap
         ref={props.onMapLoad}
         defaultZoom={16}
-        defaultCenter={{ lat: 37.76757, lng: -122.49427}}
-
-      >
+        defaultCenter={{ lat: 37.769403, lng: -122.49}}
+        >
         { userKeys.map((userKey, index) => {
           const user = users[userKey];
-          console.log(user)
           return (
             <Marker
+              key={index}
               {...user}
             />
           )
         }) }
 
-        <OverlayView
-         // position={{ lat: 37.76757, lng: -122.49427 }}
-          bounds={{
-            ne: { lat: 37.768582, lng: -122.489251 },
-            sw: { lat: 37.776576, lng: -122.495774 }
-          }}  
-          // bounds={{
-          //   north: 37.800471,
-          //   south: 37.681819,
-          //   east: -122.305608,
-          //   west: -122.587132
-          // }}
-          mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-        >
-
-          <div style={STYLES.overlayView}>
-            <h1>OverlayView</h1>
-            
-          </div>
-        </OverlayView>
+        <GroundOverlay
+          image='https://s3-us-west-2.amazonaws.com/elasticbeanstalk-us-west-2-307385678134/outside_lands.png'
+          opacity={0.7}
+          imageBounds={{
+            north: 37.771614,
+            south: 37.765553, //6118
+            east: -122.481277,
+            west: -122.496346
+          }
+        }/>
 
       </GoogleMap>
     ));
-
-
-// ///////////////////
-
-
-// function getPixelPositionOffset(width, height) {
-//   return { x: -(width / 2), y: -(height / 2) };
-// }
-
-// const OverlayViewExampleGoogleMap = withGoogleMap(props => (
-//   <GoogleMap
-//     defaultZoom={8}
-//     defaultCenter={{ lat: 37.76757, lng: -122.49427 }}
-//   >
-//     <OverlayView
-//      // position={{ lat: 37.76757, lng: -122.49427 }}
-//       bounds={{
-//         ne: { lat: 37.800471, lng: -122.305608 },
-//         sw: { lat: 37.681819, lng: -122.587132 }
-//       }}
-//       /*
-//        * An alternative to specifying position is specifying bounds.
-//        * bounds can either be an instance of google.maps.LatLngBounds
-//        * or an object in the following format:
-
-//        */
-//       /*
-//        * 1. Specify the pane the OverlayView will be rendered to. For
-//        *    mouse interactivity, use `OverlayView.OVERLAY_MOUSE_TARGET`.
-//        *    Defaults to `OverlayView.OVERLAY_LAYER`.
-//        */
-//       mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
-//       /*
-//        * 2. Tweak the OverlayView's pixel position. In this case, we're
-//        *    centering the content.
-//        */
-//       getPixelPositionOffset={getPixelPositionOffset}
-//       /*
-//        * 3. Create OverlayView content using standard React components.
-//        */
-//     >
-//       <div style={STYLES.overlayView}>
-//         <h1>OverlayView</h1>
-//       </div>
-//     </OverlayView>
-//   </GoogleMap>
-
-//   ///////////////////////
-
-
-
-
 
     return (
       <div style={{height: `100%`}}>
@@ -178,66 +105,6 @@ class MapViewer extends Component {
     );
   }
 }
-
-// class MapViewer extends React.Component {
-//   render() {
-//     const users = this.props.users;
-//     const userID = this.props.userID;
-//     const userKeys = Object.keys(users);
-
-//     return (
-//       <View>
-//         <MapView
-//           provider={MapView.PROVIDER_GOOGLE}
-//           style={localStyles.map}
-//           initialRegion={{
-//             latitude: 37.76757,
-//             longitude: -122.49427,
-//             latitudeDelta: 0.0222,
-//             longitudeDelta: 0.0121,
-//           }}>
-//           {this.props.geoFences.map((geoFence, index) => (
-//             <MapView.Circle
-//               key={index}
-//               center = {{
-//                 latitude: geoFence.latitude,
-//                 longitude: geoFence.longitude
-//               }}
-//               radius={geoFence.radius}
-//               fillColor="rgba(0, 0, 0, 0.2)"
-//               strokeColor="rgba(0, 0, 0, 0.2)"
-//             />
-//           ))}
-//           {this.props.geoFences.map((geoFence, index) => (
-//             <MapView.Marker
-//               key={index}
-//               coordinate={{
-//                 latitude: geoFence.latitude,
-//                 longitude: geoFence.longitude
-//               }}
-//               title={geoFence.name}
-//             />
-//           ))}
-          // {userKeys.map((userKey, index) => {
-          //   const user = users[userKey];
-            
-          //   return (
-          //     <MapView.Marker
-          //       key={index}
-          //       coordinate={{
-          //         latitude: user.coordinates.latitude,
-          //         longitude: user.coordinates.longitude
-          //       }}
-          //       title={user.name}
-          //       description={"Add code to determine stage"}
-          //     />
-          //   );
-          // })}
-//         </MapView>
-//       </View>
-//     );
-//   }
-// }
 
 export default connect((store) => {
   return {
