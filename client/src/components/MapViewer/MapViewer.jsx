4a4dd93a2caa39781e6a93e1543986d7d42929d5
Patrick Sheehan <psheehan89@gmail.com>
2017-02-25 09:51:54 -0800

    merging conflicts

--- client/src/components/MapViewer/MapViewer.jsx
+++ client/src/components/MapViewer/MapViewer.jsx
@@ -1,11 +1,15 @@
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker, OverlayView } from 'react-google-maps';
+import { withGoogleMap, GoogleMap, Marker, OverlayView, InfoWindow } from 'react-google-maps';
 import Helmet from 'react-helmet';
 import GroundOverlay from '../../../../GroundOverlay';
 import localStyles from './MapStyles.css';
 
 class MapViewer extends Component {
+
+  // shouldComponentUpdate() {
+  //    return false;
+  // }
 
   render() {
     const users = this.props.users;
@@ -21,6 +25,8 @@ class MapViewer extends Component {
         >
         {userKeys.map((userKey, index) => {
           const user = users[userKey];
+          user.showInfo = false;
+          console.log('userMap', user)
           const icon = {
             url: user.img,
             scaledSize: new google.maps.Size(30, 30),
@@ -31,8 +37,17 @@ class MapViewer extends Component {
           return (
             <Marker
               key={index}
-              {...user}
-              icon={icon}>
+              { ...user }
+              icon={icon}
+              onClick={() => {
+                user.showInfo = true
+                console.log('clicked', user.showInfo)
+              }}>
+              {user.showInfo && 
+                <InfoWindow onCloseClick={ () => user.showInfo = false }>
+                  <div>{user.label}</div>
+                </InfoWindow>
+              }
             </Marker>
           )
         })}
@@ -62,6 +77,10 @@ class MapViewer extends Component {
     );
   }
 
+  onClick(user) {
+    user.showInfo = true;
+  }
+
   handleMapLoad(map) {
     this._mapComponent = map;
     if (map) map.getZoom();
@@ -73,3 +92,34 @@ export default connect((store) => {
     users: store.location.users,
   };
 })(MapViewer);
+
+
+
+
+const PopUpInfoWindowExampleGoogleMap = withGoogleMap(props => (
+  <GoogleMap
+    defaultZoom={4}
+    center={props.center}
+  >
+    {props.markers.map((marker, index) => (
+      <Marker
+        key={index}
+        position={marker.position}
+        onClick={() => props.onMarkerClick(marker)}
+      >
+        {/*
+          Show info window only if the 'showInfo' key of the marker is true.
+          That is, when the Marker pin has been clicked and 'onCloseClick' has been
+          Successfully fired.
+        */}
+        {marker.showInfo && (
+          <InfoWindow onCloseClick={() => props.onMarkerClose(marker)}>
+            <div>{marker.infoContent}</div>
+          </InfoWindow>
+        )}
+      </Marker>
+    ))}
+  </GoogleMap>
+));
+
+
