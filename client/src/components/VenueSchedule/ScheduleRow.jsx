aae655fd68147ff06b593d5e79ea357a0bc8afdc
Smriti Goel <smritigoel@Smritis-MacBook-Pro.local>
2017-02-24 17:08:03 -0800

    styling schedule

--- client/src/components/VenueSchedule/ScheduleRow.jsx
+++ client/src/components/VenueSchedule/ScheduleRow.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
+import firebase from 'firebase';
+//import totem from '../../../img/radar-icon.png';
+// console.log(totem);
+// /client/src/components/VenueSchedule/ScheduleRow.jsx
+// /client/img
 // import { Image, Text, View, Button } from 'react-native';
 // import styles from '../../styles';
-// import VenueStyles from './VenueStyles';
+ import localStyles from './VenueStyles.css';
+function here(name,startTime,endTime,geofence,day) {
 
-const ScheduleRow = ({ name, startTime, endTime, geoFences, day }) => (
-  <div>
-    <div>
-      <div>{name}</div>
-      <div>
-        <div>{geoFences}</div>
-        <div>
-          {startTime.slice(0,-6)+" "+startTime.slice(startTime.length-2)+" "+
-           " - "+endTime.slice(0,-6)+" "+endTime.slice(endTime.length-2)
+  let uid = firebase.auth().currentUser.uid;
+  const db = firebase.database();
+  const agendaId = db.ref().child(`users/${ uid }/agenda`).push().key;
+  const updates = {};
+  console.log("agendaId: ",agendaId);
+  updates[`users/${ uid }/agenda/${agendaId}`] = {
+    name: name,
+    startTime: startTime,
+    endTime: endTime,
+    geoFences: geofence,
+    day: day
+  };
+  db.ref().update(updates);
+}
+
+const ScheduleRow = ({ name, startTime, endTime, geofence, day }) => (
+  // <button onClick={here.bind(null,name,startTime,endTime,geofence,day)}>
+  <div className={localStyles.gRow + " clearfix"}>
+      <img src='./img/totem.png'/>
+       <p>
+        <span className="h4">{name}</span>
+        <br/>
+          {
+            startTime.slice(0,-6)+" "+startTime.slice(startTime.length-2)+" "+
+            " - "+endTime.slice(0,-6)+" "+endTime.slice(endTime.length-2)
           }
-        </div>
-      </div>
-    </div>
-    <div/>
+      </p>
   </div>
 );
 
