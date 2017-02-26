90e97487f7cf380de024723aa319e0a4268fd3ff
Derek Young <dthomasy@gmail.com>
2017-02-25 17:49:28 -0800

    Update venueReducer.js

--- client/src/redux/reducers/venueReducer.js
+++ client/src/redux/reducers/venueReducer.js
@@ -3,50 +3,6 @@ const defaults = {
   text: '',
   groupJoined: '',
   createGroup: '',
-  group:{
-    pendingInvites: {
-    	'asdfdsaffds' : 'chuck'
-    }
-  },
-  selectedId: '',
-  venues: [
-    { id: 'osl123',
-      name: 'Outside Lands Music and Art Festival',
-      address: {
-        line1: 'Golden Gate Park',
-        line2: '',
-        line3: '',
-        city: 'San Francisco',
-        state: 'CA',
-        zip: '94122',
-        country: 'USA'
-      }
-    },
-    { id: 'coach123',
-      name: 'Coachella Valley Music and Arts Festival',
-      address: {
-        line1: 'Empire Polo Club',
-        line2: '81-800 Avenue 51',
-        line3: '',
-        city: 'Indio',
-        state: 'CA',
-        zip: '92201',
-        country: 'USA'
-      }
-    },
-    { id: 'burn123',
-      name: 'Burning Man',
-      address: {
-        line1: 'Black Rock Desert',
-        line2: '',
-        line3: '',
-        city: 'Black Rock Desert',
-        state: 'NV',
-        zip: '',
-        country: 'USA'
-      }
-    }
-  ],
   friendsList: '',
   venues: mockVenueData
 };
