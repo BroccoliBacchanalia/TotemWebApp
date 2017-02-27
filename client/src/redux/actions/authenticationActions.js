9cfc01068a74ded2ec17903e09e6cb0c58f9376b
John Currie <johntcurrie@gmail.com>
2017-02-27 14:37:36 -0800

    Added firebase id to incoming user data

--- client/src/redux/actions/authenticationActions.js
+++ client/src/redux/actions/authenticationActions.js
@@ -67,26 +67,27 @@ function getFriends() {
     let faceBookFriends = data.data;
     let firebaseArray = [];
     let firebaseData = {};
+    let temp = {};
+
     for(let key in databaseGroup[0]) {
+      databaseGroup[0][key]['firebaseId'] = key
       firebaseArray.push(databaseGroup[0][key])
     }
     firebaseData['data'] = firebaseArray;
     let friendsWithAccounts = {
       data: []
     }
-    console.log('FIRE', firebaseData)
     for (let i = 0; i < firebaseData.data.length-1; i++) {
       for (let x = 0; x < faceBookFriends.data.length-1; x++) {
         if (firebaseData.data[i].label === faceBookFriends.data[x].name) {
-          friendsWithAccounts.data.push(faceBookFriends.data[x]);
+          friendsWithAccounts.data.push(faceBookFriends.data[i]);
         }
       }
     }
-    console.log(friendsWithAccounts)
 
     store.dispatch({type: 'UPDATE_FRIENDS', friends: friendsWithAccounts})
   }).catch((error) => {
-    console.log('Error getting friends from facebook');
+    console.log('Error getting friends from facebook', error);
   })
 }
 
@@ -111,7 +112,8 @@ export function signIn() {
           email: email,
           lastTimeLoggedIn: firebase.database.ServerValue.TIMESTAMP,
           agenda: {null: "null"},
-          showInfo: true
+          showInfo: true,
+          pendingInvites: '',
         });
 
 
