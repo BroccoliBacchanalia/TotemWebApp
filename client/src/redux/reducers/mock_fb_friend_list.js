268dc026f6585e9f597ad7d363f3b64046f315f0
John Currie <johntcurrie@gmail.com>
2017-02-27 16:37:52 -0800

    Pushed all checked firebase ids into an array

--- client/src/redux/reducers/mock_fb_friend_list.js
+++ client/src/redux/reducers/mock_fb_friend_list.js
@@ -1,7 +1,9 @@
-module.exports = [
+module.exports = {
+  data: [
   {
     id: 'adkdjfkjlejw',
     name: 'Patrick',
+    firebaseId: 'sdfasdfsddfs',
     picture: {
       data: {
         url: 'https://facebook.github.io/react/img/logo_og.png'
@@ -11,6 +13,7 @@ module.exports = [
   {
     id: 'fojejflkwaj',
     name: 'John',
+    firebaseId: 'sdafasdfasdfadsf',
     picture: {
       data: {
         url: 'https://facebook.github.io/react/img/logo_og.png'
@@ -19,6 +22,7 @@ module.exports = [
   },
   {
     id: 'dlajflkand',
+    firebaseId: 'sdafasdfasdfadsf',
     name: 'Smriti',
     picture: {
       data: {
@@ -28,11 +32,13 @@ module.exports = [
   },
   {
     id: 'dldlekejdd',
+    firebaseId: 'sdafasdfasdfadsf',
     name: 'Derek',
     picture: {
       data: {
         url: 'https://facebook.github.io/react/img/logo_og.png'
       }
     }
-  },
-];
+  }
+  ]
+};
