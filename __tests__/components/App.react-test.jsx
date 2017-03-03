9950bc36429c458ea99275bce6d3d1068d7a7a97
Colorado <dthomasy@gmail.com>
2017-03-03 15:10:50 -0800

    App test suite

--- __tests__/components/App.react-test.jsx
+++ __tests__/components/App.react-test.jsx
@@ -1,16 +1,49 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { shallow, mount, render } from 'enzyme';
-import AgendaRow from '../../client/src/components/VenueSchedule/AgendaRow';
+import App from '../../client/src/components/App';
 
+const test = () => <div>hi</div>;
 
-const props = { itemKey, name, startTime, endTime, geofence, day, imgurl };
+function setup() {
+  const props = {
+    map: mockVenueData["-KdmcqUff2U8vDv-qfC1"].map
+  }
 
-describe('TEST', () => {
+  const setBasecamp = jest.fn();
+  const enzymeWrapper = shallow(<test {...props} />);
 
+  return {
+    props,
+    enzymeWrapper,
+    setBasecamp
+  }
+}
 
-  it('should render without exploding', () => {
-    expect(shallow(<AgendaRow />).length).toBeTruthy();
-  })
-})
+describe('App', () => {
+  it('should render Google Map iframe', () => {
+    const { enzymeWrapper, setBasecamp, props } = setup();
 
+    console.log(props, 'props');
+    console.log(enzymeWrapper, 'enzymeWrapper');
+    console.log(setBasecamp, 'setBasecamp');
+    expect(true).toBe(true);
+
+    // expect(enzymeWrapper.find('header').hasClass('header')).toBe(true)
+    //
+    // expect(enzymeWrapper.find('h1').text()).toBe('todos')
+    //
+    // const todoInputProps = enzymeWrapper.find('TodoTextInput').props()
+    // expect(todoInputProps.newTodo).toBe(true)
+    // expect(todoInputProps.placeholder).toEqual('What needs to be done?')
+  });
+
+  // it('should call addTodo if length of text is greater than 0', () => {
+  //   const { enzymeWrapper, props } = setup()
+  //   const input = enzymeWrapper.find('TodoTextInput')
+  //   input.props().onSave('')
+  //   expect(props.addTodo.mock.calls.length).toBe(0)
+  //   input.props().onSave('Use Redux')
+  //   expect(props.addTodo.mock.calls.length).toBe(1)
+  // });
+});
