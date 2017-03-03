90e8b4e16a6d7d756550c76b5068d7ee4e73acf9
Patrick Sheehan <psheehan89@gmail.com>
2017-03-03 14:10:14 -0800

    (fix) syntax errors in appReducer.react-test.jsx

--- __tests__/redux/reducers/appReducer.react-test.js
+++ __tests__/redux/reducers/appReducer.react-test.js
@@ -27,33 +27,33 @@ describe('Application State Reducer', () => {
   })
   describe('Toggling sidebar menu', () => {
     test('should change the "visible" state to true when false', () => {
-      expect(appReducer(undefined, {type: 'OPEN_MENU'}).visible);
+      expect(appReducer(undefined, {type: 'OPEN_MENU'}).visible)
       .toBeTruthy();
     })
     test('should change the "visible" state to false when true', () => {
-      expect(appReducer(undefined, {type: 'CLOSE_MENU'}).visible);
+      expect(appReducer(undefined, {type: 'CLOSE_MENU'}).visible)
       .toBeFalsy();
     })
   })
   describe('Toggling emergency friends modal', () => {
     test('should change the "friendsModal" state to true false', () => {
-      expect(appReducer(undefined, {type: 'TOGGLE_EMERGENCY_FRIENDS'}).friendsModal);
+      expect(appReducer(undefined, {type: 'TOGGLE_EMERGENCY_FRIENDS'}).friendsModal)
       .toBeTruthy();
     })
     test('should change the "friendsModal" state to false when true', () => {
       const testState = { friendsModal: true };
-      expect(appReducer(testState, {type: 'TOGGLE_EMERGENCY_FRIENDS'}).friendsModal);
+      expect(appReducer(testState, {type: 'TOGGLE_EMERGENCY_FRIENDS'}).friendsModal)
       .toBeFalsy();
     })
   })
   describe('Toggling emergency services modal', () => {
     test('should change the "emergencyModal" state to true when false', () => {
-      expect(appReducer(undefined, {type: 'TOGGLE_EMERGENCY_SERVICES'}).emergencyModal);
+      expect(appReducer(undefined, {type: 'TOGGLE_EMERGENCY_SERVICES'}).emergencyModal)
       .toBeTruthy();
     })
     test('should change the "emergencyModal" state to false when true', () => {
       const testState = { emergencyModal: true };
-      expect(appReducer(testState, {type: 'TOGGLE_EMERGENCY_SERVICES'}).emergencyModal);
+      expect(appReducer(testState, {type: 'TOGGLE_EMERGENCY_SERVICES'}).emergencyModal)
       .toBeFalsy();
     })
   })
