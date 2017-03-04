7e95f1026ca4e0648c55e06c220cac9b2cccd863
sam201994 <smritigoel93@gmail.com>
2017-03-03 19:30:09 -0800

    schedule reducer test

--- __tests__/redux/reducers/venueScheduleReducer.react-test.js
+++ __tests__/redux/reducers/venueScheduleReducer.react-test.js
@@ -1,14 +1,36 @@
 import venueScheduleReducer from '../../../client/src/redux/reducers/venueScheduleReducer.js';
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import * as actions from '../../../client/src/redux/actions/venueScheduleActions';
+console.log("_____________________________________________________________________________");
 describe('Venue Schedule Reducer', () => {
 
-	test('Should have set defaults', () => {
+	 test('Should have set defaults', () => {
 	  expect(venueScheduleReducer(undefined, {})).toEqual({
 		  agenda: [],
 		  selectedDay: null,
 		  selectedStage: 'All Stages'
 	  })
   })
+  test('Should be able change day', () => {
+    expect(
+      venueScheduleReducer(undefined, {
+        type: 'UPDATE_DAY',
+        payload: {
+          day: 'friday'
+        }
+      }).selectedDay
+    ).toBeTruthy()
+  })
+   test('Should be able change stage', () => {
+    expect(
+      venueScheduleReducer(undefined, {
+        type: 'UPDATE_STAGE',
+        payload: {
+          stage: 'sutro'
+        }
+      }).selectedStage
+    ).toBeTruthy()
+  })
+  
 })
