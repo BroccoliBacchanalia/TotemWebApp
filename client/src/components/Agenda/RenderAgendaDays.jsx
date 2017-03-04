4471b1906b9477f2ff54d0a2c91032dca0fdfb51
sam201994 <smritigoel93@gmail.com>
2017-03-04 00:01:27 -0800

    fixed agenda

--- client/src/components/Agenda/RenderAgendaDays.jsx
+++ client/src/components/Agenda/RenderAgendaDays.jsx
@@ -1,23 +1,27 @@
 import React from 'react';
 import localStyles from './AgendaStyles.css';
-import { updateDay } from '../../redux/actions/venueScheduleActions.js'
-import { connect } from 'react-redux';
+/* Actions */
+import { updateStage, updateDay } from '../../redux/actions/venueScheduleActions.js';
 
-const RenderAgendaDays = ({ venueSchedule}) => (
-  <nav className="navbar navbar-inverse">
-        {venueSchedule.days.map((item, key) =>
-          <div className={localStyles.navB}
-              key={key}
-              title={item}
-              onClick = {updateDay.bind(null,item)}
-              value={item}>{item}
-          </div>
-        )}
+const RenderAgendaDays = ({ days }) => (
+   <nav>
+    <select
+      id="days-dropdown"
+      className="ui selection fluid dropdown"
+      onChange={updateValue.bind(this, 'days-dropdown')}>
+      {Object.keys(days).map((day, i) => (
+        <option key={i} value={days[day]}>{day}</option>
+      ))}
+    </select>
   </nav>
 );
 
-export default connect((store) => {
-  return {
-    venueSchedule: store.venueSchedule
-  };
-})(RenderAgendaDays);
+function updateValue(id, e) {
+  const value = document.getElementById(id).value;
+  return id === 'days-dropdown' ? updateDay(value) : updateStage(value);
+}
+
+export default RenderAgendaDays;
+
+
+
