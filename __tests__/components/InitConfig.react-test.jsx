90060ea1b6960a9ad5cc576b291f5c841781b72f
John Currie <johntcurrie@gmail.com>
2017-03-06 16:56:24 -0800

    Added two initConfig tests: shows the right amount of groups and changes group id on click

--- __tests__/components/InitConfig.react-test.jsx
+++ __tests__/components/InitConfig.react-test.jsx
@@ -3,22 +3,34 @@ import store from '../../client/src/redux/store.js';
 import { mount, shallow, render } from 'enzyme';
 import { connect, mapStateToProps} from 'react-redux';
 import InviteFriends from '../../client/src/components/InitConfig/InviteFriends.jsx';
+import { ChooseGroup } from '../../client/src/components/InitConfig/ChooseGroup.jsx'
 import mockFBFriends from '../../client/src/redux/reducers/mock_fb_friend_list.js';
-import * as actions from '../../client/src/redux/actions'
+import * as actions from '../../client/src/redux/actions';
+import * as firebase from 'firebase';
+
+function setup() {
+  ChooseGroup.prototype.removeGroupFromPendingInvites = function(id) {
+    console.log('id', id)
+    store.dispatch({
+    type: 'UPDATE_USER_GROUP_ID',
+    payload: { id }
+  });
+  };
+}
 
 describe('InviteFriends', () => {
 
-	const dummyData = {
-	  uid: null,
-	  name: null,
-	  groupId: null,
-	  groupName: '',
-	  pendingInvites: {},
-	  friendList: mockFBFriends,
-	  dataRetrieved: false
-	}
+  const dummyData = {
+    uid: null,
+    name: null,
+    groupId: null,
+    groupName: '',
+    pendingInvites: {},
+    friendList: mockFBFriends,
+    dataRetrieved: false
+  }
 
-	const friends = shallow(<InviteFriends store={ store } />)
+  const friends = shallow(<InviteFriends store={ store } />)
 
   it('should render without exploding', () => {
     expect(friends.length).toBeTruthy();
@@ -29,19 +41,31 @@ describe('InviteFriends', () => {
   });
 
   it('expects a list of friends in friendList', () => {
-  	var changeId = friends.simulate('change', {preventDefault() {}, target: { name: 'uid', value: mockFBFriends} })
+    var changeId = friends.simulate('change', {preventDefault() {}, target: { name: 'uid', value: mockFBFriends} })
     expect(actions.updateUserData(mockFBFriends.data)).toEqual({type: 'UPDATE_USER_DATA', payload: mockFBFriends});
   });
 
 })
 
-describe('Initial Routes', () => {
+describe('Choose Group', () => {
+setup();
+const props = {
+  groupList: {
+    '23234342423': 'testGroup',
+    '23412344332': 'testGroup2'
+  }
+}
 
-  xit('should render map if the user is signed in, has a group, and a venue', () => {
-    actions.updateUserGroupId('12342143cc')
-    actions.updateVenueName('2341234')
-    actions.signInSuccess('1243', '123444')
+  const chooseGroup = shallow(<ChooseGroup groupList={ props.groupList } updateUserGroupID={ function(){} }/>)
 
+  it('should display list of invites to user', () => {
+    let listOfGroups = chooseGroup.find('.link');
+    expect(listOfGroups.length).toEqual(2);
   })
 
+  it('when clicked adds a groupid to user', () => {
+    let oneGroup = chooseGroup.find('#select').first();
+    oneGroup.simulate('click')
+    expect(store.getState().user.groupId.length).toBeGreaterThan(1)
+  })
 })
