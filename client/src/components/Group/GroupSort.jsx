c5459044a33f813e171ad3ca672bff7692e19536
Derek <dthomasy@gmail.com>
2017-03-06 16:47:17 -0800

    Remove overflow of button sort divs to fix mobile view

--- client/src/components/Group/GroupSort.jsx
+++ client/src/components/Group/GroupSort.jsx
@@ -13,7 +13,11 @@ const SortGroup = () => {
     <Grid columns={3} divided>
       <Grid.Row className={localStyles.sortRow}>
           {icons.map((icon, index) => (
-            <Grid.Column key={index} className={localStyles.icon}>
+            <Grid.Column
+              key={index}
+              className={localStyles.icon}
+              style={{ width: window.innerWidth / 3 }}
+            >
               <Button
                 className={localStyles.gButton}
                 className={localStyles.sort}
