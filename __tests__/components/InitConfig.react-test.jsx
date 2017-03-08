1be2592a6885d2b69365e265279ea5c5265489b5
John Currie <johntcurrie@gmail.com>
2017-03-07 18:58:03 -0800

    Added additional init config tests

--- __tests__/components/InitConfig.react-test.jsx
+++ __tests__/components/InitConfig.react-test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import store from '../../client/src/redux/store.js';
 import { mount, shallow, render } from 'enzyme';
 import { connect, mapStateToProps} from 'react-redux';
-import InviteFriends from '../../client/src/components/InitConfig/InviteFriends.jsx';
+import { InviteFriends } from '../../client/src/components/InitConfig/InviteFriends.jsx';
 import { ChooseGroup } from '../../client/src/components/InitConfig/ChooseGroup.jsx';
 import { ChooseVenue } from '../../client/src/components/InitConfig/ChooseVenue.jsx'
 import mockFBFriends from '../../client/src/redux/reducers/mock_fb_friend_list.js';
@@ -21,18 +21,18 @@ describe('InviteFriends', () => {
     dataRetrieved: false
   }
 
-  const friends = shallow(<InviteFriends store={ store } />)
+  const friends = shallow(<InviteFriends store={ store } props={ dummyData } friendList={ dummyData.friendList.data }/>)
 
   it('should render without exploding', () => {
     expect(friends.length).toBeTruthy();
   })
 
   it('expects props to be initialize upon load', () => {
-    expect(friends.props().store.getState().user.uid).toEqual(null);
+    expect(store.getState().user.uid).toEqual(null);
   });
 
   it('expects a list of friends in friendList', () => {
-    var changeId = friends.simulate('change', {preventDefault() {}, target: { name: 'uid', value: mockFBFriends} })
+    let changeId = friends.simulate('change', {preventDefault() {}, target: { name: 'uid', value: mockFBFriends} })
     expect(actions.updateUserData(mockFBFriends.data)).toEqual({type: 'UPDATE_USER_DATA', payload: mockFBFriends});
   });
 
