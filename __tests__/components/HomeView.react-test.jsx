5c254a8a54f4888e48d7c25d43e8981fbd2f8adc
John Currie <johntcurrie@gmail.com>
2017-03-08 19:48:48 -0800

    Removed extraneous tests

--- __tests__/components/HomeView.react-test.jsx
+++ __tests__/components/HomeView.react-test.jsx
@@ -6,7 +6,6 @@ import store from '../../client/src/redux/store';
 import { HomeView } from '../../client/src/components/HomeView';
 import Loading from '../../client/src/components/Auth/Loading';
 import SignInButton from '../../client/src/components/Auth/SignInButton';
-import ChooseGroup from '../../client/src/components/InitConfig/ChooseGroup';
 import ChooseVenue from '../../client/src/components/InitConfig/ChooseVenue';
 import MapViewer from '../../client/src/components/MapViewer/MapViewer';
 
@@ -44,16 +43,6 @@ describe('HomeView', () => {
       expect(wrapper.unrendered.props.auth.isUserSignedIn).toEqual(true);
       expect(wrapper.unrendered.props.user.dataRetrieved).toEqual(false);
       expect(wrapper.find(Loading).length).toEqual(1);
-    });
-
-    it('should render ChooseGroup when auth.isUserSignedIn is true, user.dataRetrieved is true, groupId does not exist, and the user has pending invites', () => {
-      const { wrapper } = setup(true, true, { 0: true });
-
-      expect(wrapper.unrendered.props.auth.isUserSignedIn).toEqual(true);
-      expect(wrapper.unrendered.props.user.dataRetrieved).toEqual(true);
-      expect(!!wrapper.unrendered.props.user.groupId).toEqual(false);
-      expect(Object.keys(wrapper.unrendered.props.user.pendingInvites).length).toBeGreaterThan(0);
-      expect(wrapper.find(ChooseGroup).length).toEqual(1);
     });
 
     it('should render ChooseVenue when auth.isUserSignedIn is true, user.dataRetrieved is true, groupId does not exist, and the user does not have pending invites', () => {
--- __tests__/components/InitConfig.react-test.jsx
+++ __tests__/components/InitConfig.react-test.jsx
@@ -2,72 +2,10 @@ import React from 'react';
 import store from '../../client/src/redux/store.js';
 import { mount, shallow, render } from 'enzyme';
 import { connect, mapStateToProps} from 'react-redux';
-import { InviteFriends } from '../../client/src/components/InitConfig/InviteFriends.jsx';
-import { ChooseGroup } from '../../client/src/components/InitConfig/ChooseGroup.jsx';
 import { ChooseVenue } from '../../client/src/components/InitConfig/ChooseVenue.jsx'
 import mockFBFriends from '../../__testConfig__/mock_fb_friend_list';
 import * as actions from '../../client/src/redux/actions';
 import * as firebase from 'firebase';
-
-describe('InviteFriends', () => {
-
-  const dummyData = {
-    uid: null,
-    name: null,
-    groupId: null,
-    groupName: '',
-    pendingInvites: {},
-    friendList: mockFBFriends,
-    dataRetrieved: false
-  }
-
-  const friends = shallow(<InviteFriends store={ store } props={ dummyData } friendList={ dummyData.friendList.data }/>)
-
-  it('should render without exploding', () => {
-    expect(friends.length).toBeTruthy();
-  })
-
-  it('expects props to be initialize upon load', () => {
-    expect(store.getState().user.uid).toEqual(null);
-  });
-
-  it('expects a list of friends in friendList', () => {
-    let changeId = friends.simulate('change', {preventDefault() {}, target: { name: 'uid', value: mockFBFriends} })
-    expect(actions.updateUserData(mockFBFriends.data)).toEqual({type: 'UPDATE_USER_DATA', payload: mockFBFriends});
-  });
-
-})
-
-describe('Choose Group', () => {
-
-ChooseGroup.prototype.removeGroupFromPendingInvites = function(id) {
-  store.dispatch({
-    type: 'UPDATE_USER_GROUP_ID',
-    payload: { id }
-  });
-};
-
-const props = {
-  groupList: {
-    '23234342423': 'testGroup',
-    '23412344332': 'testGroup2'
-  }
-}
-
-  const chooseGroup = shallow(<ChooseGroup groupList={ props.groupList } pendingInvites={ function(){} }/>)
-
-  it('should display list of invites to user', () => {
-    let listOfGroups = chooseGroup.find('.link');
-    expect(listOfGroups.length).toEqual(2);
-  })
-
-  it('when clicked adds a groupid to user', () => {
-    let oneGroup = chooseGroup.find('#select').first();
-    oneGroup.simulate('click')
-    expect(store.getState().user.groupId.length).toBeGreaterThan(1)
-  })
-
-})
 
 describe('Choose Venue', () => {
   const venues = {
