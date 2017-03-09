bd690730ca53099f6c9c555e52985c61a335478b
Derek <dthomasy@gmail.com>
2017-03-08 22:54:44 -0800

    (FEAT) Group: create group watching functionality

--- client/src/components/Nav/PlaceTotem.jsx
+++ client/src/components/Nav/PlaceTotem.jsx
@@ -7,7 +7,8 @@ import { placeTotemOnClick, updateMeetupTime } from '../../redux/actions/groupAc
 import localStyles from './UtilStyles.css';
 
 const PlaceTotem = ({ app, venue }) => {
-  const minuteOptions = ['00', '05'];
+  const hourOptions = ['', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+  const minuteOptions = ['', '00', '05'];
   for (let i = 10; i < 60; i += 5) {
     minuteOptions.push(i);
   }
@@ -27,20 +28,20 @@ const PlaceTotem = ({ app, venue }) => {
           <br />
           <span>(Optional)</span> Set a meetup time. The totem will expire 30 minutes after your set time.
           <div className={localStyles.center + ' ' + localStyles.time}>
-            <select>
-              {['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'].map((num) => (
+            <select name="meetup-hours">
+              {hourOptions.map((num) => (
                 <option key={num} value={num}>{num}</option>
               ))}
             </select>
             :
-            <select>
+            <select name="meetup-minutes">
               {minuteOptions.map((num) => (
                 <option key={num} value={num}>{num}</option>
               ))}
             </select>
-            <select>
-              <option value='AM'>AM</option>
-              <option value='PM'>PM</option>
+            <select name="meridiem">
+              <option value='0'>AM</option>
+              <option value='12'>PM</option>
             </select>
           </div>
         </div>
@@ -61,9 +62,16 @@ const PlaceTotem = ({ app, venue }) => {
           size='huge'
           inverted
           onClick={() => {
-            // let time = document.querySelector('input[name="meetup-time"]').value;
-            // if (time.length > 0) time = convertToDate(time);
-            // updateMeetupTime(time);
+            const meridiem = document.querySelector('select[name="meridiem"]').value;
+            const mins = document.querySelector('select[name="meetup-minutes"]').value;
+            let hours = document.querySelector('select[name="meetup-hours"]').value;
+            let time = '';
+
+            if (hours.length > 0 && mins.length > 0) {
+              hours = Number(meridiem) + Number(hours);
+              time = convertToDate(hours, Number(mins));
+            }
+            updateMeetupTime(time);
             placeTotemOnClick(true);
             toggleTotemModal(false);
           }}>
@@ -74,10 +82,8 @@ const PlaceTotem = ({ app, venue }) => {
   );
 }
 
-function convertToDate(time) {
+function convertToDate(hours, minutes) {
   let expiresAt = new Date();
-  const hours = Number(time.split(':')[0]);
-  const minutes = Number(time.split(':')[1]);
   const setTime = hours + minutes / 60;
   const currentTime = expiresAt.getHours() + (expiresAt.getMinutes() / 60);
 
