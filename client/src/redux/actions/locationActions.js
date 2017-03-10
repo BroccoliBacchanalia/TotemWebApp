eb292e233e3525394c39ad7f860db3748bd1523d
Derek <dthomasy@gmail.com>
2017-03-09 20:12:26 -0800

    Geofence is now stored in FB

--- client/src/redux/actions/locationActions.js
+++ client/src/redux/actions/locationActions.js
@@ -14,39 +14,50 @@ export function addUserListener(userId) {
 //Grabs the location of the current user and updates firebase
 export function geolocate() {
   function success(pos) {
-    console.log(pos);
     const uid = store.getState().user.uid;
-    const geofence = getGeofence(pos.coords);
+
+    /********************* TESTING ONLY *********************/
+    console.log(pos);
+    let lat = pos.coords.latitude;
+    let lng = pos.coords.longitude;
     const hrLat = 37.7837693;
     const hrLng = -122.4090728;
 
-    firebaseSet(`users/${uid}/geofence`, geofence);
-
-    if (uid === 'KrSypCuwkBdEiH2JAJgOGxZN8m52') {
-      firebaseSet(`users/${uid}/position`, {
-        timestamp: pos.timestamp,
-        lat: pos.coords.latitude - (hrLat - 33.679914), // Sahara
-        lng: pos.coords.longitude - (hrLng - (-116.236626)) // Sahara
-      });
-    } else if (uid === 'BSxDfzp6vwdLEP0g5xqjXpL6zDF3') {
-      firebaseSet(`users/${uid}/position`, {
-        timestamp: pos.timestamp,
-        lat: pos.coords.latitude - (hrLat - 33.6809343), // Heineken House
-        lng: pos.coords.longitude - (hrLng - (-116.238377)) // Heineken House
-      });
-    } else if (uid === 'nJU5dy4GSjgirgWMENAYeCQYMcG2') {
-      firebaseSet(`users/${uid}/position`, {
-        timestamp: pos.timestamp,
-        lat: pos.coords.latitude - (hrLat - 33.6829),
-        lng: pos.coords.longitude - (hrLng - (-116.2383))
-      });
-    } else {
-      firebaseSet(`users/${uid}/position`, {
-        timestamp: pos.timestamp,
-        lat: pos.coords.latitude  - (hrLat - 33.684409), // Coachella stage
-        lng: pos.coords.longitude - (hrLng - (-116.239769)) // Coachella stage
-      });
+    switch(uid) {
+      case 'KrSypCuwkBdEiH2JAJgOGxZN8m52': {
+        lat -= (hrLat - 33.679914); // Sahara
+        lng -= (hrLng - (-116.236626)); // Sahara
+        break;
+      }
+      case 'BSxDfzp6vwdLEP0g5xqjXpL6zDF3': {
+        // lat -= (hrLat - 33.6798); // Sahara
+        // lng -= (hrLng - (-116.236626)); // Sahara
+        lat -= (hrLat - 33.6809343); // Heineken House
+        lng -= (hrLng - (-116.238377)); // Heineken House
+        break;
+      }
+      case 'nJU5dy4GSjgirgWMENAYeCQYMcG2': {
+        lat -= (hrLat - 33.6829);
+        lng -= (hrLng - (-116.2383));
+        break;
+      }
+      default : {
+        lat  -= (hrLat - 33.684409); // Coachella stage
+        lng -= (hrLng - (-116.239769)); // Coachella stage
+      }
     }
+    /*****************************************************/
+
+    // const lat = pos.coords.latitude; // uncoment in prod
+    // const lng = pos.coords.longitude; // uncoment in prod
+    const geofence = getGeofence({ lat, lng });
+
+    firebaseSet(`users/${uid}/geofence`, geofence);
+    firebaseSet(`users/${uid}/position`, {
+      timestamp: pos.timestamp,
+      lat,
+      lng
+    });
   }
 
   function error(err) {
