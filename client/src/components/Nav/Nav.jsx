ad38b42a81e16b95361facbb312c0d9a34124049
Patrick Sheehan <psheehan89@gmail.com>
2017-04-07 00:38:01 -0700

    Updated test version number

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -49,7 +49,7 @@ const Nav = ({ app, user }) => {
           className={ chat.className }>
           <Icon name={ chat.iconName } /> { chat.displayName }
         </Menu.Item>
-        <p><span style={{ color: 'white' }}>Test v0.5.1</span></p>
+        <p><span style={{ color: 'white' }}>Test v0.5.2</span></p>
       </Sidebar>
       <Sidebar.Pusher>
         {!!user.groupId ? <HeaderBlock /> : ''}
